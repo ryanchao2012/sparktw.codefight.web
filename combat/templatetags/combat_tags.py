cd3057a595cf1926ed9896c5ccb1b9ba7a82704f
Ryan Chao <ryanchao2012@gmail.com>
2017-09-24 05:01:36 +0000

    regular save

--- combat/templatetags/combat_tags.py
+++ combat/templatetags/combat_tags.py
@@ -5,10 +5,15 @@ register = template.Library()
 
 
 @register.filter
-def pretty(td):
+def pretty_duration(td):
     total_seconds = int(td.total_seconds())
     hours = total_seconds // 3600
     minutes = (total_seconds - hours * 3600) // 60
     seconds = (total_seconds - hours * 3600 - minutes * 60)
 
     return '{} hr {} min {} sec'.format(hours, minutes, seconds)
+
+@register.filter
+def pretty_title(tt):
+    return tt.title()
+
